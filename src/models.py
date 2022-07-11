1fc80cb78416fb476f1192f28d51633a1da110a0
Manuel Martret <manulm27@hotmail.com>
2022-07-11 19:31:19 +0000

    assembly data model instagram

--- src/models.py
+++ src/models.py
@@ -10,21 +10,35 @@ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    
+class followers(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('person.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('person.id'), primary_key=True)
+    user = relationship(Person)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('person.id'))
+
+class comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey('person.id'))
+    text_comment = Column(String(500))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class media(Base):
+    __tablename__= 'media'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
 
     def to_dict(self):
         return {}
